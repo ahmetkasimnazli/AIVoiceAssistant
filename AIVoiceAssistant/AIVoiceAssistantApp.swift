5116c9c9d3116b37e891bd2866ad802d7dff9756
ahmetkasimnazli <ahmetnazl2@gmail.com>
2023-12-21 18:00:50 +0200

    Initial Commit

---
+++ AIVoiceAssistant/AIVoiceAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIVoiceAssistantApp.swift
+//  AIVoiceAssistant
+//
+//  Created by Ahmet Kasım Nazlı on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AIVoiceAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIVoiceAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIVoiceAssistant
+//
+//  Created by Ahmet Kasım Nazlı on 21.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
